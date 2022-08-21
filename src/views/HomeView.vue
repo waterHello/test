0fff040c4e8bb0893751ee4be96478dc241014ff
waterHello <111680614+waterHello@users.noreply.github.com>
2022-08-21 16:43:50 +0800

    Add files via upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <van-button loading
+                type="primary" />
+    <van-button loading
+                type="primary"
+                loading-type="spinner" />
+    <van-button loading
+                type="info"
+                loading-text="加载中..." />
+  </div>
+</template>
+
